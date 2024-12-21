4ed3ced1e981b787bd3182945426dee34a9b1080
yashushashank <shashankml000@gmail.com>
2024-12-21 13:17:56 +0530

    Add files via upload

---
+++ sh.js
@@ -0,0 +1,32 @@
+
+
+console.log("age")
+
+let age = 20
+
+console.log(age)
+
+let age1 =  10
+let height = undefined
+
+console.log("******************")
+
+console.log("well come to js" , typeof(age))
+console.log("well to IT FILD",typeof(height))
+
+console.log("******************")
+
+let city = "bengaluru".toUpperCase()
+
+console.log(city .length)
+
+let des = `As per the provision of Article 79 of Indian Constitution, the House of
+ the People, the Lok Sabha is the lower House of the parliament. Lok Sabha is composed of ...`
+
+ console.log(des.lastIndexOf(`Lok`));
+
+//  let x = 10
+//  let y = 20
+
+//  console.log(x+y)
+ 
